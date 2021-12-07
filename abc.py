f1d3eef1f1e91cf0878bffb26c6703b8be91fcf8
MUNIASAMY <94515894+samypgt@users.noreply.github.com>
2021-12-07 18:58:30 +0530

    abc as filename
    area of circle

--- abc.py
+++ abc.py
@@ -1,3 +1,5 @@
-file=input("enter the file name  ")
-
-print("The extension of the file is:'python' ")
+r=float(input("Enter the radius of the circle:"))      
+area=3.14*r*r
+print("The area of the circle with radius 1.1 is :", area)
+filename=input("Enter the filename as abc.py:")
+print("The extension of the file is : python")
