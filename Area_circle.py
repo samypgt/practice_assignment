6c618b5c15fcc5e231038f8d085d5e2a02929b94
MUNIASAMY <94515894+samypgt@users.noreply.github.com>
2021-12-07 08:29:19 +0530

    Update Area_circle.py

--- Area_circle.py
+++ Area_circle.py
@@ -2,5 +2,4 @@ print("Input the radius of the circle:")
 r=1.1
 area=3.14 *r*r
 print("The area of the circle with radius 1.1 is :", area)
-exten='py'
-print("The extension of this file is",exten)
+
