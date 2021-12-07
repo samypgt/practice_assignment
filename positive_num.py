0e25cb0451d35f806275324996d7f42dbda2c763
MUNIASAMY <94515894+samypgt@users.noreply.github.com>
2021-12-07 20:49:08 +0530

    positivenum
    display only positive numbers

---
+++ positive_num.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,14]
+list2=[12,14,-95,3]
+print(list1)
+print(list2)
+for num in list1:
+    if num>=0:
+        print(num,end=" ")
+print()
+for num in list2:
+    if num>=0:
+        print(num,end=" ")
